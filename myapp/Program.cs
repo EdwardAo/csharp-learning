9d9d1a3083e8d55be1be3656d7c3d3e99427599e
Edward Ao <87841176+EdwardAo@users.noreply.github.com>
2021-10-21 18:34:41 -0700

    first commit

---
+++ myapp/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] names = { "Bob", "Joe", "Will", "Bob", "Will", "Joe", "Joe", "Mavi", "Edward" };
+string[] greetings = { "Hello", "Goodbye", "Gutentag", "Aufweidersehen", "Bonjour", "Au revoir", "Hola", "Adios", "Bonjorno", "Ciao" };
+foreach (string name in names)
+{
+    foreach (string greeting in greetings)
+    {
+        Console.WriteLine(greeting + " " + name + "!");
+    }
+}
